84aedf8d0d63cb0bc06ac67ecb10ca2757a1b9a2
Samiul <samiulhossain1337@gmail.com>
2019-11-02 02:43:56 +0600

    another video done

--- script.js
+++ script.js
@@ -36,12 +36,16 @@ var personProto = {
 };
 
 var john = Object.create(personProto);
+john.name = 'John' ;
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-
-
-
-
-
+var jane = Object.create(personProto,
+{
+    name: {value : 'Jane' },
+    yearOfBirth: {value : 1969 },
+    job: {value: 'designer' }
+})
 
 
 
