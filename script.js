bad4fe5a8f94bbcddf999c2bc34f3a2d5d7cc2c6
Samiul <samiulhossain1337@gmail.com>
2019-11-02 03:57:14 +0600

    video for morning, Number - 8. for now another done

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ mark.calculateAge();
 
 **/
 
-// Object.create
+/* Object.create
+
 var personProto = {
     calcAge: function() {
         console.log(2019 - this.yearOfBirth);
@@ -46,6 +47,60 @@ var jane = Object.create(personProto,
     yearOfBirth: {value : 1969 },
     job: {value: 'designer' }
 })
+
+*/
+
+
+//Primitives vs Objects
+
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age=30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+//Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a,b){
+    a=30;
+    b.city='San Francisco';
+}
+
+change(age,obj);
+
+console.log(age);
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
