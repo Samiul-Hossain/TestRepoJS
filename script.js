16057bcf7086afcd1214c716e23c0c9d548bb29f
Samiul <samiulhossain1337@gmail.com>
2019-11-02 18:26:15 +0600

    video 9 done

--- script.js
+++ script.js
@@ -92,7 +92,8 @@ console.log(obj.city);
 
 
 */
-// Function passing
+ /*
+//////////////Function passing
 
 var years = [1990,1965,1937,2005,1997];
 
@@ -128,6 +129,33 @@ var rates = arrayCalc(ages,maxHeartRate);
 
 console.log(rates);
 
+*/
+
+function interviewQuestion(job){
+    if (job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    }
+    else if(job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, '+name+'?');
+        }
+    }
+    else{
+        return function(name){
+            console.log('Hello '+name+ ', what do you do?');
+        }
+    }
+}
+
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
 
 
 
